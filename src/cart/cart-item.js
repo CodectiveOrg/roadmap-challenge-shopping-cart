81b9255933240a9ec6156debe0eaf52f85a2404c
SinaMAmini <smobasheramini@gmail.com>
2025-08-13 09:49:35 +0330

    [] cart-item

--- src/cart/cart-item.js
+++ src/cart/cart-item.js
@@ -1,1 +1,28 @@
-export class CartItem {}
+import { Product } from "../product/product.js";
+
+export class CartItem {
+  #product;
+  #quantity;
+  constructor(product, quantity) {
+    this.#product = new Product(product?.name, product?.price);
+    this.#quantity(quantity);
+  }
+
+  get quantity() {
+    return this.#quantity;
+  }
+
+  get product() {
+    return this.#product;
+  }
+
+  set quantity(quantity) {
+    if (this.#quantity > 0) {
+      this.#quantity = quantity;
+    }
+  }
+
+  get subtotal() {
+    return this.#product.price * this.#quantity;
+  }
+}
--- src/product/product.js
+++ src/product/product.js
@@ -5,9 +5,13 @@ export class Product {
   constructor(name, price) {
     if (typeof name === "string" && name !== "") {
       this.#name = name.trim();
+    } else {
+      throw new Error(`Product name must be a string.`);
     }
     if (typeof price === "number" && !isNaN(price)) {
       this.#price = price;
+    } else {
+      throw new Error(`Product price must be a number.`);
     }
   }
 
