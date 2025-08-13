75fdf90a3b37dd4e5eb775ea8f66b36a7a795853
SinaMAmini <smobasheramini@gmail.com>
2025-08-13 09:18:41 +0330

    [] product

--- src/product/product.js
+++ src/product/product.js
@@ -1,1 +1,21 @@
-export class Product {}
+export class Product {
+  #name;
+  #price;
+
+  constructor(name, price) {
+    if (typeof name === "string" && name !== "") {
+      this.#name = name.trim();
+    }
+    if (typeof price === "number" && !isNaN(price)) {
+      this.#price = price;
+    }
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  get price() {
+    return this.#price;
+  }
+}
