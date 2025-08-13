7c7265233f370981ce99fc84717d19ca581b7cec
SinaMAmini <smobasheramini@gmail.com>
2025-08-13 10:21:52 +0330

    [] cart

--- src/cart/cart.js
+++ src/cart/cart.js
@@ -1,4 +1,5 @@
 import { Product } from "../product/product.js";
+import { CartItem } from "./cart-item.js";
 
 export class Cart {
   #items = [];
@@ -16,6 +17,23 @@ export class Cart {
       throw new Error("Product must be an instance of Product class.");
     } else if (!Number.isInteger(quantity)) {
       throw new Error("Quantity must be a positive integer.");
+    } else {
+      if (this.#items.find((item) => item.product.name === product.name)) {
+        quantity++;
+      } else {
+        this.#items.push(new CartItem(product, quantity));
+      }
     }
   }
+
+  removeProduct(product) {
+    if (!Product.productValidation(product)) {
+      throw new Error("Product must be an instance of Product class.");
+    } else {
+      /// bet this won't work
+      this.#items.splice(this.#items.indexOf(product), 1);
+    }
+  }
+
+  applyDiscount(strategy) {}
 }
