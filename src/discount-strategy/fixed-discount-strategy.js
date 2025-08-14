241869aee239eaad38dee35e7518f19176d02f13
ZahraZare <zahrazare3099@gmail.com>
2025-08-14 17:25:56 +0330

    refactor calculate method

--- src/discount-strategy/fixed-discount-strategy.js
+++ src/discount-strategy/fixed-discount-strategy.js
@@ -16,10 +16,14 @@ export class FixedDiscountStrategy extends DiscountStrategy {
 
   calculate(cart) {
     if (cart instanceof Cart) {
-      if (cart.items.length > 0 && cart.total > this.#amount) {
-        const presentageDiscount = new PercentageDiscountStrategy(cart.total);
-        const res = cart.total - presentageDiscount.total;
-        return res;
+      if (cart.items.length === 0) {
+        return 0;
+      }
+      if (cart.total > this.#amount) {
+        return this.#amount;
+      }
+      if (this.#amount > cart.total) {
+        return cart.total;
       }
     } else {
       throw new Error("Cart must be an instance of Cart class.");
