9e87aa654537c84003270698a4f85150de0cace3
BijanProgrammer <bijaneisapour@gmail.com>
2025-08-16 11:19:58 +0330

    [ADD] CartItem

--- src/cart/cart-item.js
+++ src/cart/cart-item.js
@@ -1,1 +1,35 @@
-export class CartItem {}
+import { Product } from "../product/product.js";
+
+export class CartItem {
+  #product;
+  #quantity;
+
+  constructor(product, quantity) {
+    if (!(product instanceof Product)) {
+      throw new Error("Product must be an instance of Product class.");
+    }
+
+    this.#product = product;
+    this.quantity = quantity;
+  }
+
+  get product() {
+    return this.#product;
+  }
+
+  get quantity() {
+    return this.#quantity;
+  }
+
+  get subtotal() {
+    return this.#product.price * this.#quantity;
+  }
+
+  set quantity(value) {
+    if (typeof value !== "number" || !Number.isInteger(value) || value <= 0) {
+      throw new Error("Quantity must be a positive integer.");
+    }
+
+    this.#quantity = value;
+  }
+}
