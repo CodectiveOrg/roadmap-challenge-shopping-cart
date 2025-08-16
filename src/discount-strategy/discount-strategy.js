7409ce2fc83dc90c56e047592f7c0be623558be7
BijanProgrammer <bijaneisapour@gmail.com>
2025-08-16 12:04:45 +0330

    [ADD] DiscountStrategy

--- src/discount-strategy/discount-strategy.js
+++ src/discount-strategy/discount-strategy.js
@@ -1,1 +1,13 @@
-export class DiscountStrategy {}
+export class DiscountStrategy {
+  constructor() {
+    if (new.target === DiscountStrategy) {
+      throw new Error(
+        "DiscountStrategy is abstract and cannot be used directly.",
+      );
+    }
+  }
+
+  calculate(cart) {
+    throw new Error("Subclasses must implement calculate method.");
+  }
+}
