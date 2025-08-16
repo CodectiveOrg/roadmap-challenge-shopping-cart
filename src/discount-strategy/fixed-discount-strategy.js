c98c7cf3ba9250efe642c7e3da2959c15cb2334b
BijanProgrammer <bijaneisapour@gmail.com>
2025-08-16 12:10:08 +0330

    [ADD] FixedDiscountStrategy

--- src/discount-strategy/fixed-discount-strategy.js
+++ src/discount-strategy/fixed-discount-strategy.js
@@ -1,1 +1,25 @@
-export class FixedDiscountStrategy {}
+import { DiscountStrategy } from "./discount-strategy.js";
+import { Product } from "../product/product.js";
+import { Cart } from "../cart/cart.js";
+
+export class FixedDiscountStrategy extends DiscountStrategy {
+  #amount;
+
+  constructor(amount) {
+    super();
+
+    if (typeof amount !== "number" || Number.isNaN(amount) || amount < 0) {
+      throw new Error("Amount must be a non-negative number.");
+    }
+
+    this.#amount = amount;
+  }
+
+  calculate(cart) {
+    if (!(cart instanceof Cart)) {
+      throw new Error("Cart must be an instance of Cart class.");
+    }
+
+    return Math.min(cart.total, this.#amount);
+  }
+}
