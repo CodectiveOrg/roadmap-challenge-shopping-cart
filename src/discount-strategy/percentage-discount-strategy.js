7fd05c45e5c393887dc17f9bd5913fa0ca47dd15
ZahraZare <zahrazare3099@gmail.com>
2025-08-17 00:57:19 +0330

    refactor precentage-dis class

--- src/discount-strategy/percentage-discount-strategy.js
+++ src/discount-strategy/percentage-discount-strategy.js
@@ -6,26 +6,28 @@ export class PercentageDiscountStrategy extends DiscountStrategy {
   constructor(percentage) {
     super();
     if (
-      typeof percentage === "number" &&
-      percentage >= 0 &&
-      percentage <= 100
+      typeof percentage !== "number" ||
+      Number.isNaN(percentage) ||
+      percentage < 0 ||
+      100 < percentage
     ) {
-      this.#percentage = percentage;
-    } else {
       throw new Error("Percentage must be a number from 0 to 100.");
     }
+    this.#percentage = percentage;
   }
 
   calculate(cart) {
-    if (cart instanceof Cart) {
-      if (this.#percentage < cart.total) {
-        const discount = cart.total * (this.percentage / 100);
-        return discount;
-      } else if (this.#percentage > cart.total) {
-        return 100;
-      }
-    } else {
+    if (!(cart instanceof Cart)) {
       throw new Error("Cart must be an instance of Cart class.");
     }
+    const discount = cart.total * (this.#percentage / 100);
+
+    // if (discount < cart.total) {
+    //   return discount;
+    // } else {
+    //   return cart.total;
+    // }
+
+    return Math.min(cart.total, discount);
   }
 }
